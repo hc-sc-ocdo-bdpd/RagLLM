2620dac309660499fd952fbefa0a51b636a915ed
Karolina Prazuch <69173333+kprazuch@users.noreply.github.com>
2024-01-31 13:49:05 -0500

    Creating PQ index

--- faiss_index.py
+++ faiss_index.py
@@ -5,14 +5,32 @@ import os
 from typing import List
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from tqdm import tqdm
-from promptflow_vectordb.core.contracts import (
-    EmbeddingModelType,
-    StorageType,
-    StoreCoreConfig,
-)
-from promptflow_vectordb.core.embeddingstore_core import EmbeddingStoreCore
 import matplotlib.pyplot as plt 
 import numpy as np 
+import faiss
+from openai import AzureOpenAI
+
+MODEL_API_VERSION = "2023-05-15"
+MODEL_DEPLOYMENT_NAME = "ada_embedding"
+DIMENSION = 1536
+M = 8
+NBITS = 8
+
+# TODO: Figure out how to incrementally add docs to index
+# TODO: Save index to local storage
+
+def get_embeddings(client: AzureOpenAI, file_name: str) -> List[int]:
+    with open(file_name, "r", encoding="utf-8") as f:
+        page_content = f.read()
+        doc_embeddings = []
+        splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=10)
+        for chunk in splitter.split_text(page_content):
+            response = client.embeddings.create(
+                model = MODEL_DEPLOYMENT_NAME, 
+                input = chunk
+            )
+            doc_embeddings.append(np.array(response.data[0].embedding))
+    return doc_embeddings
 
 def create_index(file_path, store_name, graph = False):
     '''
@@ -27,41 +45,21 @@ def create_index(file_path, store_name, graph = False):
     local_file_path = file_path
 
     # Configure and create an embedding store
-    MODEL_API_VERSION = "2023-05-15"
-    MODEL_DEPLOYMENT_NAME = "ada_embedding"
-    DIMENSION = 1536
-    os.environ["Azure_OpenAI_MODEL_ENDPOINT"] = "<ENTER ENDPOINT HERE>"
-    os.environ["Azure_OpenAI_MODEL_API_KEY"] = "<ENTER API KEY>"
-
-    # Configure an embedding store to store index file
-    store_path = os.path.join(os.getcwd(), store_name)
-    config = StoreCoreConfig.create_config(
-        storage_type=StorageType.LOCAL,
-        store_identifier=store_path,
-        model_type=EmbeddingModelType.AOAI,
-        model_api_base=os.environ["Azure_OpenAI_MODEL_ENDPOINT"],
-        model_api_key=os.environ["Azure_OpenAI_MODEL_API_KEY"],
-        model_api_version=MODEL_API_VERSION,
-        model_name=MODEL_DEPLOYMENT_NAME,
-        dimension=DIMENSION,
-        create_if_not_exists=True,
+    client = AzureOpenAI(
+        azure_deployment = MODEL_DEPLOYMENT_NAME, 
+        api_version = MODEL_API_VERSION,
+        azure_endpoint = os.environ["OPENAI_MODEL_ENDPOINT"],
+        # openai_api_type = "azure",
+        api_key = os.environ["OPENAI_API_KEY"]
     )
-    store = EmbeddingStoreCore(config)
 
-    # Split document to chunks, embed chunks and create Faiss index
-    def get_file_chunks(file_name: str) -> List[str]:
-        with open(file_name, "r", encoding="utf-8") as f:
-            page_content = f.read()
-            chunks = []
-            splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=10)
-            for chunk in splitter.split_text(page_content):
-                chunks.append(chunk)
-            return chunks
-        
     # Count the total number of files for the progress bar
     total_files = sum([len(files) for _, _, files in os.walk(local_file_path)])
     progress_bar = tqdm(total=total_files, desc="Processing Files")
     elapsed_times = []
+    chunks = []
+    metadatas = []
+    embeddings = []
  
     for root, _, files in os.walk(local_file_path):
         for file in files:
@@ -71,20 +69,22 @@ def create_index(file_path, store_name, graph = False):
                 title = f.readline().strip('\n')
 
             # Split the file into chunks.
-            chunks = get_file_chunks(each_file_path)
-            count = len(chunks)
-            metadatas = [
+            embeddings.extend(get_embeddings(client, each_file_path))
+            # TODO: Update count
+            count = len(embeddings)
+            metadatas.extend([
                 {"title": title, "source": os.path.join(file)}
-            ] * count
+            ] * count)
 
-            # Embed chunks into embeddings, generate index in embedding store.
-            # If your data is large, inserting too many chunks at once may cause
-            # rate limit error，you can refer to the following link to find solution
-            # https://learn.microsoft.com/en-us/azure/cognitive-services/openai/quotas-limits
-            store.batch_insert_texts(chunks, metadatas)
             progress_bar.update()
             if graph:
                 elapsed_times.append(progress_bar.format_dict['elapsed'])
+
+    xb = np.array(embeddings)
+    # print(xb.shape)
+
+    index = faiss.IndexPQ(DIMENSION, M, NBITS)
+    index.train(xb)
 
     progress_bar.close()
     # create line graph
@@ -96,5 +96,6 @@ def create_index(file_path, store_name, graph = False):
         plt.xlabel("# of Documents Indexed") 
         plt.ylabel("Elapsed Time (seconds)")
         plt.title("# of Documents Indexed versus Elapsed Time")
-        plt.show() 
-    return store
+        plt.show()
+
+    return index
