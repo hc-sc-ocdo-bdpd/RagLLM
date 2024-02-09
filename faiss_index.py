513e5231d736baba0b9baef3dd6ad04a79d8578a
Karolina Prazuch <69173333+kprazuch@users.noreply.github.com>
2024-02-09 13:27:42 -0500

    fixed incremental additions & .faiss file creation

--- faiss_index.py
+++ faiss_index.py
@@ -2,11 +2,18 @@
 import os
 from typing import List
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.docstore.base import AddableMixin, Docstore
+from langchain.docstore.in_memory import InMemoryDocstore
 from tqdm import tqdm
 import matplotlib.pyplot as plt 
 import numpy as np 
 import faiss
 from openai import AzureOpenAI
+from pathlib import Path
+import pickle
+import uuid
+import random
 
 MODEL_API_VERSION = "2023-05-15"
 MODEL_DEPLOYMENT_NAME = "ada_embedding"
@@ -14,15 +21,15 @@ DIMENSION = 1536
 M = 8
 NBITS = 8
 
-# TODO: Figure out how to incrementally add docs to index -> read in index and then add
-# TODO: Save index to local storage -> testing .faiss file -> check if same as other .faiss file
+# TODO: Save index to local storage -> figure out docstore - try local_docstore?
 # https://docs.llamaindex.ai/en/latest/examples/vector_stores/FaissIndexDemo.html#
 
 # Create chunks and embeddings
-def get_embeddings(client: AzureOpenAI, file_name: str) -> List[int]:
+def get_embeddings(client: AzureOpenAI, file_name: str):
     with open(file_name, "r", encoding="utf-8") as f:
         page_content = f.read()
         doc_embeddings = []
+        chunks = []
         splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=10)
         for chunk in splitter.split_text(page_content):
             response = client.embeddings.create(
@@ -30,10 +37,13 @@ def get_embeddings(client: AzureOpenAI, file_name: str) -> List[int]:
                 input = chunk
             )
             doc_embeddings.append(np.array(response.data[0].embedding))
-    return doc_embeddings
+            # CREATE RANDOM EMBEDDINGS FOR TESTING:
+            # doc_embeddings.append(np.array([random.random()] * DIMENSION))
+            chunks.append(chunk)
+    return doc_embeddings, chunks
 
 # Create PQ index
-def create_index(file_path, store_name, graph = False):
+def create_index(file_path, folder_path, graph = False):
     '''
     Creates a Faiss index for a specific set of documents.
     Args:   file_path (str): path to the folder of documnts to be indexed.
@@ -49,7 +59,6 @@ def create_index(file_path, store_name, graph = False):
         azure_deployment = MODEL_DEPLOYMENT_NAME, 
         api_version = MODEL_API_VERSION,
         azure_endpoint = os.environ["OPENAI_MODEL_ENDPOINT"],
-        # openai_api_type = "azure",
         api_key = os.environ["OPENAI_API_KEY"]
     )
 
@@ -69,8 +78,9 @@ def create_index(file_path, store_name, graph = False):
                 title = f.readline().strip('\n')
 
             # Create embeddings
-            new_embeddings = get_embeddings(client, each_file_path)
+            new_embeddings, new_chunks = get_embeddings(client, each_file_path)
             embeddings.extend(new_embeddings)
+            chunks.extend(new_chunks)
             count = len(new_embeddings)
             metadatas.extend([
                 {"title": title, "source": os.path.join(file)}
@@ -82,24 +92,55 @@ def create_index(file_path, store_name, graph = False):
 
     # Create array of embeddings
     xb = np.array(embeddings)
-    # print(xb.shape)
+
+    path = Path(folder_path)
+    path.mkdir(exist_ok=True, parents=True)
+    
+    # Update Docstore
+    documents = [Document(page_content=t, metadata=m) for t, m in zip(chunks, metadatas)]
 
     # Train index and add embeddings to it
-    index = faiss.IndexPQ(DIMENSION, M, NBITS)
-    if not index.is_trained:
+    index_file = Path(str(path / "index.faiss"))
+    pkl_file = Path(str(path / "index.pkl"))
+
+    if not index_file.is_file() or not pkl_file.is_file():
+        print("Index does not exist")
+        index = faiss.IndexPQ(DIMENSION, M, NBITS)
         index.train(xb)
-    index.add(xb)
+        index.add(xb)
+        # TODO: Update pickle file
+        # index_to_docstore_id = {i: str(uuid.uuid4()) for i in range(len(documents))}
+        # docstore = InMemoryDocstore(
+        #     {index_to_docstore_id[i]: doc for i, doc in enumerate(documents)}
+        # )
+    else:
+        print("Index exists")
+        index = faiss.read_index(str(path / "index.faiss"))
+        index.add(xb)
+        # TODO: Figure out docstore
+        # docstore = Docstore
+        # docstore.add({id_: doc for id_, doc in zip(ids, documents)})
+        # with open(str(path / "index.pkl"), 'rb') as f:
+        #     pkl_file = pickle.load(f)
+        # index_to_docstore_id = pkl_file[1]
+        # starting_len = len(index_to_docstore_id)
+        # index_to_id = {starting_len + j: str(uuid.uuid4()) for j in range(len(documents))}
+        # index_to_docstore_id.update(index_to_id)
 
     # Write index to local file
-    print(index.ntotal)
-    faiss.write_index(index, 'new_faiss_index_store/index.faiss')
+    print("Index size:", index.ntotal)
+    # print("Pkl size:", len(index_to_docstore_id))
+
+    faiss.write_index(index, str(path / "index.faiss"))
+
+    # with open(path / "index.pkl", "wb") as f:
+    #     pickle.dump((docstore, index_to_docstore_id), f)
 
     progress_bar.close()
     # create line graph
     if graph:  
         x = np.array(range(progress_bar.format_dict['total']))
         y = np.array(elapsed_times)
-        
         plt.plot(x, y) 
         plt.xlabel("# of Documents Indexed") 
         plt.ylabel("Elapsed Time (seconds)")
