4661b332cee711345e6205625b437841c6830471
TylerWilsonHC-SC <tyler.wilson@hc-sc.gc.ca>
2024-06-21 15:27:23 -0700

    tested the performance of the number of cluster hyperparameter

--- index_IVFFlat.py
+++ index_IVFFlat.py
@@ -4,9 +4,9 @@ import faiss
 class IndexIVFFlat(FaissIndex):
     def __init__(self) -> None:
         super().__init__()
-        self.NLIST = 64
 
-    def create(self, input_path: str, output_path: str):
+    def create(self, input_path: str, output_path: str, n_list: int):
+        self.NLIST = n_list
         quantizer = faiss.IndexFlatL2(self.DIMENSION)
         index_constructor = faiss.IndexIVFFlat(quantizer, self.DIMENSION, self.NLIST)
         super().create(index_constructor, input_path, output_path)
